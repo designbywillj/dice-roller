727bfc8312067ccef68b896dd19726c74307173f
Willy Laptop <zapddesign@gmail.com>
2020-01-30 21:40:49 -0600

    Updated variabled

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,22 +1,22 @@
-var diceWrapper = document.getElementById('dice-wrapper')
-var submit = document.getElementById('submit')
+let diceWrapper = document.getElementById('dice-wrapper')
+let submit = document.getElementById('submit')
 
-var dice = [4, 6, 8, 10, 12, 20]
+let dice = [4, 6, 8, 10, 12, 20]
 
 function generateButtons() {
     for (i = 0; i < dice.length; i++) {
-        var btnCol = document.createElement('div')
+        let btnCol = document.createElement('div')
         btnCol.classList.add('col-4')
         diceWrapper.insertBefore(btnCol, submit)
 
-        var btnRadio = document.createElement('input')
+        let btnRadio = document.createElement('input')
         btnRadio.type = 'radio'
         btnRadio.name = 'dice'
         btnRadio.id = 'd' + dice[i]
         btnRadio.value = dice[i]
         btnCol.appendChild(btnRadio)
 
-        var btnLabel = document.createElement('label')
+        let btnLabel = document.createElement('label')
         btnLabel.setAttribute('for', btnRadio.id)
         btnLabel.innerText = btnRadio.id.toUpperCase()
         btnLabel.classList.add('dice__btn')
@@ -25,14 +25,12 @@ function generateButtons() {
 }
 generateButtons()
 
-var submit = document.getElementById('submit')
 submit.addEventListener('click', function() {
-    var radio = document.getElementsByName('dice')
+    let radio = document.getElementsByName('dice')
     for (i = 0; i < radio.length; i++) { 
         if (radio[i].checked) {
-            var resultText = document.getElementById('result')
+            let resultText = document.getElementById('result')
             resultText.innerText = Math.floor(Math.random() * radio[i].value + 1)
-            result.appendChild(resultText)
         }
     }
 })
